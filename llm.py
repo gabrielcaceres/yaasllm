d80415ec95a41dcc490d94bcd14dafff06a89fbd
Gabriel Caceres <gacs86@gmail.com>
2023-09-03 11:11:46 -0400

    Reformat and add type hints

--- llm.py
+++ llm.py
@@ -1,20 +1,34 @@
+from typing import Any
 import openai
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 
 
-def msg(prompt: str, role: str = "user", **kwargs) -> dict:
+def msg(
+        prompt: str,
+        role: str = "user",
+        **kwargs: Any
+) -> dict[str, Any]:
     return dict(role=role, content=prompt, **kwargs)
 
 
 class OpenAIChatModel:
 
-    def __init__(self, model="gpt-3.5-turbo", system_prompt="", **kwargs):
+    def __init__(
+            self,
+            model: str = "gpt-3.5-turbo",
+            system_prompt: str = "",
+            **kwargs: Any
+    ) -> Any:
         self.model = model
         self.system_prompt = system_prompt
         self.openai_args = dict(model=model, **kwargs)
         self.last_run = None
 
-    def generate(self, messages, **kwargs):
+    def generate(
+            self,
+            messages: list[dict[str, str]],
+            **kwargs: Any
+    ) -> list[str]:
         openai_args = self.openai_args.copy()
         openai_args.update(kwargs)
         response_object = openai.ChatCompletion.create(messages=messages, **openai_args)
@@ -27,7 +41,11 @@ class OpenAIChatModel:
         )
         return responses
 
-    def __call__(self, prompt, **kwargs):
+    def __call__(
+            self,
+            prompt: str,
+            **kwargs: Any
+    ) -> list[str]:
         messages = [msg(self.system_prompt, "system"),
                     msg(prompt, "user")]
         responses = self.generate(messages, **kwargs)

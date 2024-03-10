d20dc16f9239ffbeb091c67acb86ee587fa0080e
Gabriel Caceres <gacs86@gmail.com>
2024-03-10 13:02:43 -0400

    Some prompt modifications and cleaning of yaml headers in generated response

--- agent.py
+++ agent.py
@@ -1,15 +1,51 @@
+from typing import Optional
 from prompt import Prompt
 from tools import yaml
+from llm import msg
 
+# answer_format = Prompt("""\
+# ```yaml
+# ---
+# Step: <step>  # iteration number starting with 0
+# Thought: > <thought>  # think step by step about what you know and what you still need to figure out
+# Plan: > <plan>  # think about what you should do next
+# Actions:  # list of actions to take, can be multiple at a time
+#   - tool_name: <tool name>  # name of tool to use
+#     tool_inputs: <tool inputs>  # list inputs to selected tool
+# ...
+# ```
+# """)
 answer_format = Prompt("""\
-Step: <step>  # iteration number starting with 0
-Thought: <thought>  # think step by step about what you know and what you still need to figure out
-Plan: <plan>  # think about what you should do next
-Action:  # list of actions to take, can be multiple at a time
-  - tool_name: <tool name>:  # name of tool to use
-    tool_inputs: <tool inputs>  # list inputs to selected tool
+```yaml
+---
+!!map
+Step:  # iteration number for the current task, starting with 0. Increase as you perform more steps, reset to 0 after reaching the final answer and starting a new task
+Knowledge: |  # review the facts you currently know and what you still need to figure out
+Plan: |  # think step by step about what you should do next
+Actions: !!seq  # list of actions to take, can be multiple at a time
+  - &{label}  # unique label to reference the output of this action
+    tool_name: !!str  # name of tool to use
+    tool_inputs: !!map  # inputs to selected tool
+...
+```
 """)
 
+example_action = Prompt("""\
+---
+Stage: 0
+Knowledge: |
+  - I don't have any information yet.
+  - I need to determine a task to perform.
+Plan: |
+  - Ask the user what to do next.
+Actions: 
+  - !Action
+    tool_name: user input
+    tool_inputs:
+      query: What can I do for you?
+...
+"""
+)
 
 sys_prompt_template = Prompt("""\
 You are a helpful assistant who relies on a set of tools to help provide answers. You will think through the question, decide on an action to take and provide that to the 'user', and then build from the observation your receive as input.
@@ -21,22 +57,22 @@ These are tools you have at your disposal for actions:
 {tool_descriptions}
 ```
 
-And this is the format your answers should follow:
-```yaml
----{answer_format}...
-```
+The following code block shows the format your answers should follow:
+{answer_format}
+
+You can use YAML anchors (`&`) to label values and aliases (`*`) to reference labels with any previous values you want to reuse. Follow the syntax from the standard YAML specification.
 
 The 'user' will provide you with the following input showing the result of your action:
 ```yaml
 ---
-Observation: <The result returned from the selected tool in Action>
+Observations: <observations> # the results returned from the selected Actions
 ...
 ```
 """)
 
 obs_prompt = Prompt("""\
 ---
-Observation: {obs}
+Observations: {obs}
 ...
 """)
 
@@ -49,6 +85,7 @@ class Agent:
         ).data
         self.llm = llm_class(system_prompt=sys_prompt)
         self.toolkit = toolkit
+        self.llm.chat_history = [msg(example_action.data, "assistant")]
 
     def run(self, query):
         msg = self.llm.chat(obs_prompt.format(obs=query).data)
@@ -65,7 +102,7 @@ class Agent:
 
     def get_actions(self, msg):
         msg_dict = self.load_msg(msg)
-        return msg_dict['Action']
+        return msg_dict['Actions']
 
     def get_tool_list(self, actions):
         return [action.get("tool_name") for action in actions]
@@ -80,8 +117,19 @@ class Agent:
             outcomes.append(tool_output)
         return outcomes
 
-    def load_msg(self, msg):
-        return yaml.load(msg)
+    def load_msg(self, msg: str):
+        clean_msg = msg.removeprefix("```")
+        clean_msg = clean_msg.removeprefix("yaml")
+        clean_msg = clean_msg.removeprefix("\n")
+        clean_msg = clean_msg.removesuffix("\n")
+        clean_msg = clean_msg.removesuffix("```")
+        print("cleaned message")
+        print(clean_msg)
+        print("end cleaned message")
+        print("Commented map")
+        print(yaml.load(clean_msg))
+        print("end Commented map")
+        return yaml.load(clean_msg)
 
     def format_obs(self, outcomes):
         obs_sep = '\n  - '
